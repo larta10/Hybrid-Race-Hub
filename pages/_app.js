fe44c0f8807326bf4a8fed02c54ad911e24e3311
larta10 <133422207+larta10@users.noreply.github.com>
2026-04-23 11:03:12 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,6 @@
-*, *::before, *::after {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
 
-html, body {
-  background: #0E0F13;
-  color: #F0F0F5;
-  font-family: system-ui, -apple-system, sans-serif;
-  min-height: 100vh;
-  width: 100%;
-  overflow-x: hidden;
-}
+import "../styles/global.css";
 
-#__next {
-  width: 100%;
-  min-height: 100vh;
-  background: #0E0F13;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
